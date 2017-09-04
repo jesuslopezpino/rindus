9c754caf14b5fff2fd8406e2fd20cd8e0841ed45
jesuslopezpino <jesus_lopez_pino@hotmail.com>
2017-09-05 00:21:34 +0200

    increase y decrease

--- rindus-microservices/src/main/java/com/rindus/microservicesdemo/web/StocksCollectionJsonController.java
+++ rindus-microservices/src/main/java/com/rindus/microservicesdemo/web/StocksCollectionJsonController.java
@@ -1,8 +1,17 @@
 package com.rindus.microservicesdemo.web;
+import com.rindus.microservicesdemo.domain.Product;
 import com.rindus.microservicesdemo.domain.Stock;
+import com.rindus.microservicesdemo.service.api.StockServiceExtended;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.roo.addon.web.mvc.controller.annotations.ControllerType;
 import org.springframework.roo.addon.web.mvc.controller.annotations.RooController;
 import org.springframework.roo.addon.web.mvc.controller.annotations.responses.json.RooJSON;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * = StocksCollectionJsonController
@@ -13,4 +22,30 @@ import org.springframework.roo.addon.web.mvc.controller.annotations.responses.js
 @RooController(entity = Stock.class, type = ControllerType.COLLECTION)
 @RooJSON
 public class StocksCollectionJsonController {
+	
+	@Autowired
+	StockServiceExtended stockServiceExtended; 
+	
+	@RequestMapping(value = "/test")
+	public String test() {
+		return "{ \"hola\": \"ey\" }";
+	}
+
+	@RequestMapping(value = "/increase")
+	public String increase(@Valid Product product, int num) {
+		String result = "{ \"increased\" : \"";
+		List<Stock> increasedList = this.stockServiceExtended.increase(product, num);
+		int increased = increasedList.size();
+		result += increased  + "\" }";
+		return result;
+	}
+
+	@RequestMapping(value = "/decrease")
+	public String decrease(@Valid Product product, int num) {
+		String result = "{ \"decreased\" : \"";
+		int decreased = this.stockServiceExtended.decrease(product, num);
+		result += decreased + "\" }";
+		return result;
+	}
+
 }
