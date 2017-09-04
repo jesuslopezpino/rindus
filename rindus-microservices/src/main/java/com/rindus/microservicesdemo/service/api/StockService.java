e7aa20edf67f72986ee65007d50df1a456f2d903
jesuslopezpino <jesus_lopez_pino@hotmail.com>
2017-09-04 03:03:30 +0200

    subimos con errores

--- rindus-microservices/src/main/java/com/rindus/microservicesdemo/service/api/StockService.java
+++ rindus-microservices/src/main/java/com/rindus/microservicesdemo/service/api/StockService.java
@@ -1,5 +1,9 @@
 package com.rindus.microservicesdemo.service.api;
+import com.rindus.microservicesdemo.domain.Product;
 import com.rindus.microservicesdemo.domain.Stock;
+
+import java.util.List;
+
 import org.springframework.roo.addon.layers.service.annotations.RooService;
 
 /**
@@ -10,4 +14,8 @@ import org.springframework.roo.addon.layers.service.annotations.RooService;
  */
 @RooService(entity = Stock.class)
 public interface StockService {
+	
+	public List<Stock> increase(Product product, int num);
+
+	public List<Stock> decrease(Product product, int num);
 }
--- rindus-microservices/src/main/java/com/rindus/microservicesdemo/service/impl/StockServiceImpl.java
+++ rindus-microservices/src/main/java/com/rindus/microservicesdemo/service/impl/StockServiceImpl.java
@@ -1,6 +1,18 @@
 package com.rindus.microservicesdemo.service.impl;
+import com.rindus.microservicesdemo.domain.Product;
+import com.rindus.microservicesdemo.domain.Stock;
 import com.rindus.microservicesdemo.service.api.StockService;
+
+import io.springlets.data.domain.GlobalSearch;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.roo.addon.layers.service.annotations.RooServiceImpl;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * = StockServiceImpl
@@ -10,4 +22,33 @@ import org.springframework.roo.addon.layers.service.annotations.RooServiceImpl;
  */
 @RooServiceImpl(service = StockService.class)
 public class StockServiceImpl implements StockService {
+
+	 @Transactional
+		public List<Stock> increase(Product product, int num) {
+			List<Stock> result = new ArrayList<Stock>();
+			for (int i = 0; i < num; i++) {
+				Stock stockItem = new Stock();
+				stockItem.setProducto(product);
+				stockItem.setDateIn(new Date());
+				result.add(stockItem);
+			}
+//			return this.getStockRepository().save(result);
+			return null;
+		}
+	    
+	    @Transactional
+	    public void decrease(Product product) {
+//			Page<Stock> currentStockProduct = getStockRepositoryCustom().findByProducto(product, null, null);
+			List<Stock> result = new ArrayList<Stock>();
+			int discarted = 0;
+//			for (Iterator iterator = currentStockProduct.iterator(); iterator.hasNext();) {
+//				Stock stock = (Stock) iterator.next();
+//				if(stock.getDateOut() == null) {
+//					discarted++;
+//					result.add(stock);
+//				}
+//			}
+//			getStockRepository().delete(result);
+	    }
+
 }
